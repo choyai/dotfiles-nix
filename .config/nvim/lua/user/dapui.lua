2e4770b72a5482f857c8a94b6719824c5d0dca4f
justinlime <justinlime1999@github.com>
2023-03-16 03:53:44 -0500

    nix

--- .config/nvim/lua/user/dapui.lua
+++
@@ -1,7 +0,0 @@
-local status, plugin = pcall(require,'dapui')
-if not status then
-    print('Error with plugin: ', plugin)
-else
-plugin.setup()
-end
-
--- .config/nvim/lua/user/lsp.lua
+++
@@ -1,9 +0,0 @@
-local status, plugin = pcall(require,'lsp-zero')
-if not status then
-    print('Error with plugin: ', plugin)
-else
-
-plugin.preset('recommended')
-plugin.setup()
-
-end
