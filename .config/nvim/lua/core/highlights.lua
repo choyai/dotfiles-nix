fbfb77f6787c5e88dd79459f2cfd820f792a5ba4
justinlime <justinlime1999@gmail.com>
2023-05-27 12:54:44 -0500

    fix

--- .config/nvim/lua/core/highlights.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-vim.api.nvim_set_hl(0, "NonText", {bg = "none"})
-vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-vim.api.nvim_set_hl(0, "LineNr", {bg = "none"})
-vim.api.nvim_set_hl(0, "SignColumn", {bg = "none"})
-vim.api.nvim_set_hl(0, "TabLine", {bg = "none"})
-vim.opt.fillchars:append { eob = " " }
-
